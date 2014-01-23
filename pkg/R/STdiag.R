b7fbe23a896e60932cfff0e8f4ace64c03fa3c71
Vincent Le Bourlot <65e01dbb@opayq.com>
2014-01-23 19:36:58 +0000

    Added a function to select and extract a point a line or a region: STdiag.measure

--- pkg/R/STdiag.R
+++ pkg/R/STdiag.R
@@ -77,6 +77,221 @@ STdiag <-
         sm=0.5
       } 
       if(sm!=0){
+        if(sm<0.1){sm=0.1}
+        
+        
+        if(missing(n)){n=floor(c(length(unique(data[[names[2]]]))*10*min(sm,1),
+                                 length(unique(data[[names[3]]]))*10*min(sm,1)
+        ))}
+        if(any(is.na(n))){n=floor(c(length(unique(data[[names[2]]]))*10*min(sm,1),
+                                    length(unique(data[[names[3]]]))*10*min(sm,1)
+        ))}
+        h1=bandwidth.nrd(data[[names[2]]])*sm
+        h2=bandwidth.nrd(data[[names[3]]])*sm
+        data=kde2dWeighted(x=data[[names[2]]],
+                           y=data[[names[3]]],
+                           w=data[[names[1]]],
+                           h=c(h1,h2),n=n,proba.min=probamin
+        )
+        main=paste("Kernel density plot:",main)
+        colnames(data)=names[c(2,3,1)]
+      }
+    }
+    
+    # If log=TRUE, transform z to 
+    if(log==T){
+      formula<-as.formula(paste("log10(",names[1],")~",names[2],"*",names[3]))  
+    }
+    
+    # Retrieve zmin and zmax from zlim, if NULL, takes range(z)
+    if(is.null(zlim)){
+      zlim=range(data[[names[1]]],na.rm=TRUE)
+    }
+    zmin=zlim[1]
+    z0=max(min(subset(data[[names[1]]],data[[names[1]]]>0)),zmin)
+    zmax=zlim[2]
+    
+    
+    
+    # Define the color scale
+    if(is.list(color)){
+      colo=color[[2]]
+      color=color[[1]]
+    }
+    col=switch(color,
+               gray = gray.colors(I(znb+10),start=0,end=1)[znb:1],
+               topo = topo.colors(I(znb+10)),
+               terrain = terrain.colors(I(znb+10)),
+               heat = heat.colors(I(znb+10)),
+               cm = cm.colors(I(znb+10)),
+               rainbow=rainbow(I(znb+10),alpha=0.8)[znb:1],
+               custom=colo,
+               tim.colors(I(znb+10))         
+    )
+    
+    # Define where to draw ticks on the colorbar.
+    if(log){
+      # Produce vector of position of ticks
+      mat=mapply(function(x) seq(1:9)*10^x, seq(floor(log10(z0)),floor(log10(zmax))))
+      at=c(mat)
+      index=which(at>=z0 & at<=zmax)
+      at=at[index]
+      char=character(length(at))
+      match=match(mat[1,],at)
+      char[match[!is.na(match)]]=format(mat[1,!is.na(match)],sci=T)
+      char[1]=format(at[1],sci=T)
+      char[length(at)]=format(at[length(at)],sci=T)
+      colKey=list(labels=list(cex=1
+                              ,at=log10(at)
+                              ,labels=char)
+      )
+      
+    }else{
+      colKey=list(labels=list(cex=1,
+                              at=seq(signif(zmin,1),signif(zmax,1),signif((zmax-zmin)/7,1))
+      )
+      )
+    }
+    
+    
+    if(is.null(colorkey)){
+      colKey=colKey
+    }else if(is.logical(colorkey) & colorkey==TRUE){
+      colKey=colKey
+    }else { #if(!is.null(colorkey))
+      colKey=c(colKey,colorkey)
+    }
+    
+    # Create vector zat to position the colors
+    if(log){
+      zat=seq(log10(z0),log10(zmax),length.out=znb)
+    }else{
+      zat=seq(zmin,zmax,length.out=znb)
+    }
+    
+    
+    scales=c(scales,list(alternating=1,
+                         tck=0.5))
+    
+    if (inherits(data[[names[2]]],c("Date","POSIXlt"))){
+      Y=diff(strptime(c("2009","2010"),format='%Y'))
+      M=diff(strptime(c("2009-09-01","2009-10-01"),format='%Y-%m-%d'))
+      D=diff(strptime(c("2009-09-01","2009-09-02"),format='%Y-%m-%d'))
+      rDate=range(data[[names[2]]])
+      minD=min(diff(unique(data[[names[2]]])))
+      maxD=diff(rDate)
+      format=NULL
+      if(maxD>D){
+        format=paste0("%d")
+      }
+      if(maxD>M){
+        format=paste0(format,"-%b")
+      }
+      if(maxD>Y){
+        format=paste0(format,"-%y")
+      }
+      scales=c(scales,list(x=list(format=format)))
+    }
+    
+    # Create the plot
+    sb <- trellis.par.get() 
+    sb.bu <- sb
+    sb$panel.background$col <- bgcolor
+    sb$axis.line$lwd <-1
+    trellis.par.set(sb) 
+    
+    lp=levelplot(formula,data,  #formula and data to plot
+                 col.regions=col,  #color scale to use
+                 at=zat,  #where to put the colors
+                 colorkey=colKey,  #where to draw the ticks on the color bar
+                 xlab=xlab,ylab=ylab, #write axes labels
+                 main=as.character(main),  #write main title
+                 scales=scales, #draw ticks inside the box
+                 ...
+    )
+    trellis.par.set(sb.bu) 
+    lp$logcol = log
+    return(lp)
+    
+  }# This function is based on the lattice function levelplot and is specifically designed to help plot structure-time diagrams
+# It takes as input a data frame of the form x, y, z, and plots z~x*y.
+# Several options are coded in the function
+#     -- main: character string, add a title to the plot
+#     -- xlab: character string, add a label to x axis
+#     -- ylab: character string, add a label to y axis
+#     -- log: boolean, if TRUE z axis (color) is on a log scale, else z axis is linear
+#     -- zlim: numeric vector, c(zmin, zmax), specify the range of color to use. If NULL, zlim=range(z)
+#     -- znb: integer, specifies the number of colors to use
+#     -- color: character string, type of color gradient to use. can be either one of gray, topo, terrain
+#               heat, tim, cm or rainbow. default is rainbow
+#     -- bgcolor: rgb(), color of the background of the plot
+
+STdiag <-
+  function(formula,data,
+           x,y,z,
+           main,
+           xlab,ylab,
+           log=FALSE,
+           zlim,znb=50,color="",
+           smooth=FALSE,sm,n,probamin=1e-6,
+           interp=FALSE,intervX,intervY,
+           bgcolor=rgb(254,254,226,maxColorValue=255),
+           scales, colorkey,
+           ...)
+  {
+    
+    # if arguments are missing, put the value to NULL
+    if (missing(formula)){cat('missing');formula <- NULL}
+    if (missing(data)){data <- NULL}
+    if (missing(x)){x <- NULL}
+    if (missing(y)){y <- NULL}
+    if (missing(z)){z <- NULL}
+    if (missing(main)){main <- NULL}
+    if (missing(xlab)){xlab <- NULL}
+    if (missing(ylab)){ylab <- NULL}
+    if (missing(zlim)){zlim <- NULL}
+    if (missing(scales)){scales <- list()}
+    if (missing(colorkey)){colorkey <- NULL}
+    
+    if(inherits(formula,"matrix")){z=formula;formula=NULL}
+    if(inherits(formula,"data.frame")){data=formula;formula=NULL}
+    if (is.null(data)&!is.null(z))
+    {
+      data=Matrix2DataFrame(z,x,y)
+      formula=NULL
+    }
+    
+    # If formula is not specified, data must be of the form x, y, z
+    if(is.null(formula) & dim(data)[2]!=3){
+      cat("ERROR: If formula is not specified, data must be of the form x y z.\n")
+      return(invisible())
+    }
+    
+    # Retrieve names of the variables to plot and create the formula z~x*y if NULL
+    if(!is.null(formula)){
+      names=all.vars(formula)
+    }else{
+      data=data[,c(3,1,2)]
+      names=colnames(data)
+      formula=as.formula(paste(names[1]," ~ ",names[2],"*",names[3],sep=""))
+    }
+    
+    if(interp){
+      if(missing(intervX)){
+        intervX=min(diff(unique(sort(data[[names[2]]]))))
+      }
+      if(missing(intervY)){
+        intervY=min(diff(unique(sort(data[[names[3]]]))))
+      }
+      data=data.frame(data[[names[2]]],data[[names[3]]],data[[names[1]]])
+      colnames(data)=names[c(2,3,1)]
+      data=Interpolation(data,intervX,intervY)
+    }
+    if(smooth){
+      if(missing(sm)){
+        sm=0.5
+      } 
+      if(sm!=0){
       if(sm<0.1){sm=0.1}
       
       
---
+++ pkg/R/STdiag.measure.R
@@ -0,0 +1,99 @@
+STdiag.measure = function(stdiag,type=c("point","line"),region=FALSE,range=3.3,pch=21,lwd=3,col.line=1,col='white',fill=1,cex=1.2){
+  graphics.off()
+  print(stdiag)
+  
+  ### som used functions
+  gety = function(x,datax,datay){
+    if(length(datay)==1){return(datay)}
+    lm=lm(datay~datax)
+    y=lm$coefficients[1]+x*lm$coefficients[2]
+    return(y)
+  }
+  
+  region = function(x,y,rx,ry,data){
+    return(
+      data[data$x>=x-rx & data$x<=x+rx & data$y>=y-ry & data$y<=y+ry,]
+    )
+  }
+  ####
+  
+  if(missing(type)) type="point"
+  # Select if we measure a point or a line
+  eval=switch(type,
+              p="point",po="point",poi="point",poin="point",point="point",
+              l="line",li="line",lin="line",line="line"
+  )
+  
+  # Recover the data
+  data=data.frame(x=stdiag$panel.args.common$x,
+                  y=stdiag$panel.args.common$y,
+                  z=stdiag$panel.args.common$z)
+  if(stdiag$logcol){data$z=exp(data$z)}
+  
+  # flag to ask if the selection is good  
+  accept=0
+  
+  # if we look for a point
+  if(eval=="point"){
+    cat("*** Click on the point you need to measure ***\n")
+    while(accept!=1){
+      # click 1 time
+      trellis.focus()
+      i<-panel.identify(n=1)
+      trellis.unfocus()
+      
+      # update the plot with the point
+      print(update(stdiag,panel=function(...){panel.levelplot(...);panel.points(data$x[i],data$y[i],pch=pch,lwd=lwd,col=col,fill=fill,cex=cex)}))
+      
+      # ask if the point is correct
+      cat("Keep the point (0/1)?")
+      accept <- scan(what=integer(1),nmax=1)
+    }
+    
+    # if the point is correct, return the index and the 
+    # corresponding values of x, y and z
+    ret = data[i,]
+    ret$index=i
+    rownames(ret)="point"
+    
+  }
+  
+  # if we look for a line
+  if(eval=="line"){
+    cat("*** Click on the beginning and end points of the line you need to measure ***\n")
+    while(accept!=1){
+      # click 2 times
+      trellis.focus()
+      i<-panel.identify(n=2)
+      trellis.unfocus()
+      
+      # update the plot with the line 
+      print(update(stdiag,panel=function(...){panel.levelplot(...);panel.lines(data$x[i],data$y[i],type="o",pch=pch,lwd=lwd,col=col.line,fill=fill,cex=cex);panel.points(data$x[i],data$y[i],pch=pch,lwd=lwd,col=col,fill=fill,cex=cex)}))
+      
+      # ask if the point is correct
+      cat("Keep the line (0/1)?")
+      accept <- scan(what=integer(1),nmax=1)
+    }
+    ret = data[i,]
+    rownames(ret)=c("start","end")
+    ret$index=i
+    ret$slope=lm(y~x,ret)$coef[2]
+    
+  }
+  
+  if(region){
+    subdata=data.frame(x=numeric(0),y=numeric(0),z=numeric(0))
+    xs=unique(data$x[data$x>=min(ret$x)&data$x<=max(ret$x)])
+    for(xi in xs){
+      yi=gety(xi,ret$x,ret$y)
+      yi=data$y[which.min(abs(yi-data$y))]
+      subdata=rbind(subdata,region(xi,yi,range*mean(diff(unique(data$x))),range*mean(diff(unique(data$y))),data))
+    }
+    print(update(stdiag,panel=function(...){panel.levelplot(...);panel.lines(data$x[i],data$y[i],type="o",pch=21,lwd=3,col=1,fill=0,cex=1.2);panel.points(data$x[i],data$y[i],pch=21,lwd=3,col='white',fill=1,cex=1.2); panel.points(subdata$x,subdata$y)}))
+    ret=list(selection=ret,region=subdata)
+  }
+  
+  return(ret)
+}
+
+
